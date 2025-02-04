5f4c93fe967dd9fff1c50d2317294a478960a38f
Brett Jones <bretttj17@gmail.com>
2025-02-04 10:21:31 -0800

    added db scaffolding

---
+++ GymBro_App/Models/FitnessChallenge.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymBro_App.Models;
+
+[Table("FitnessChallenge")]
+public partial class FitnessChallenge
+{
+    [Key]
+    [Column("ChallengeID")]
+    public int ChallengeId { get; set; }
+
+    [StringLength(255)]
+    public string? ChallengeName { get; set; }
+
+    public string? Description { get; set; }
+
+    public DateOnly? StartDate { get; set; }
+
+    public DateOnly? EndDate { get; set; }
+
+    [StringLength(255)]
+    public string? Goal { get; set; }
+
+    public string? Prize { get; set; }
+
+    [InverseProperty("Challenge")]
+    public virtual ICollection<Leaderboard> Leaderboards { get; set; } = new List<Leaderboard>();
+
+    [ForeignKey("ChallengeId")]
+    [InverseProperty("Challenges")]
+    public virtual ICollection<User> Users { get; set; } = new List<User>();
+}
---
+++ GymBro_App/Models/Food.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymBro_App.Models;
+
+[Table("Food")]
+public partial class Food
+{
+    [Key]
+    [Column("FoodID")]
+    public int FoodId { get; set; }
+
+    [StringLength(255)]
+    public string? FoodName { get; set; }
+
+    public int? CaloriesPerServing { get; set; }
+
+    public int? ProteinPerServing { get; set; }
+
+    public int? CarbsPerServing { get; set; }
+
+    public int? FatPerServing { get; set; }
+
+    [StringLength(50)]
+    public string? ServingSize { get; set; }
+
+    [ForeignKey("FoodId")]
+    [InverseProperty("Foods")]
+    public virtual ICollection<Meal> Meals { get; set; } = new List<Meal>();
+}
