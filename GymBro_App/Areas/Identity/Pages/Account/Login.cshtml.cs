39a01aa7be88d0a7615e1ad9273754b356f49914
Angel Larios <alarios21@mail.wou.edu>
2025-02-07 13:52:47 -0800

    Changed the login page to require username instead of email

--- GymBro_App/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ GymBro_App/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -65,14 +65,6 @@ namespace GymBro_App.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [EmailAddress]
-            public string Email { get; set; }
-
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
-            [Required]
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
@@ -82,6 +74,9 @@ namespace GymBro_App.Areas.Identity.Pages.Account
             /// </summary>
             [Display(Name = "Remember me?")]
             public bool RememberMe { get; set; }
+
+            [Required]
+            public string Username { get; set; }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
@@ -111,7 +106,7 @@ namespace GymBro_App.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
