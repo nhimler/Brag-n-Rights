6ebbe418a47bc849564f3704688069ba8123c55b
Shdomter <124474116+Shdomter@users.noreply.github.com>
2025-02-09 20:57:40 -0800

    added selection removal, styling to food search page

--- GymBro_App/wwwroot/js/foodSearch.js
+++ GymBro_App/wwwroot/js/foodSearch.js
@@ -31,46 +31,62 @@ async function displaySearchResults(result){
     heading.textContent = "Results";
     resultList.appendChild(heading);
 
-    let ul = document.createElement("ul");
+    let table = document.createElement("table");
+    table.className = "table table-bordered";
+    tbody = document.createElement("tbody");
     result.forEach(food => {
         if(!findValueInChildren(selectedFood, food.foodId)){
             // console.log("foodId: " + food.foodId + " not found in selectedFood");
-            let li = document.createElement("li");
-            li.value = food.foodId;
-            li.textContent = food.foodName;
+            let tr = document.createElement("tr");
+            let td = document.createElement("td");
+            td.colspan = 2;
+            td.value = food.foodId;
+            td.textContent = food.foodName;
             let expandBtn = document.createElement("button");
             expandBtn.className = "btn";
             expandBtn.textContent = "v";
+            expandBtn.type = "button";
             expandBtn.addEventListener("click", function(){
                 if(this.textContent === "v"){
                     this.textContent = "^";
                     let info = document.createElement("p");
                     info.textContent = food.foodDescription;
-                    li.appendChild(info);
+                    td.appendChild(info);
                 }else{
                     this.textContent = "v";
-                    li.removeChild(li.lastChild);
+                    td.removeChild(td.lastChild);
                 }
             });
 
+            td.appendChild(expandBtn);
+
+            let td2 = document.createElement("td");
             let addBtn = document.createElement("button");
             addBtn.className = "btn";
             addBtn.textContent = "Add";
+            addBtn.type = "button";
             addBtn.addEventListener("click", function(){
                 if(this.textContent === "Add"){
-                    movedLi = this.parentElement;
-                    this.parentElement.remove();
+                    this.textContent = "Remove";
+                    movedLi = this.parentElement.parentElement;
+                    this.parentElement.parentElement.remove();
                     selectedFood.appendChild(movedLi);
                 }else{
+                    this.textContent = "Add";
+                    movedLi = this.parentElement.parentElement;
+                    this.parentElement.parentElement.remove();
+                    tbody.prepend(movedLi);
 
                 }
             });
-            li.appendChild(expandBtn);
-            li.appendChild(addBtn);
-            ul.appendChild(li);
+            td2.appendChild(addBtn);
+            tr.appendChild(td);
+            tr.appendChild(td2);
+            tbody.appendChild(tr);
+            table.appendChild(tbody);
         }
     });
-    resultList.appendChild(ul);
+    resultList.appendChild(table);
 }
 
 function findValueInChildren(parent, value){
