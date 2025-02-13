09fa0f899b26f23771ab262995a0ec8e42af7f28
Angel Larios <alarios21@mail.wou.edu>
2025-02-13 12:30:37 -0800

    Updated view model 'ViewModels/UserInfoModel' for user page. Set up UserPageController's Index() method to user the view model.

--- GymBro_App/Controllers/UserPageController.cs
+++ GymBro_App/Controllers/UserPageController.cs
@@ -1,21 +1,40 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using GymBro_App.Models;
+using GymBro_App.DAL.Abstract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using GymBro_App.ViewModels;
 
 namespace GymBro_App.Controllers;
 
 public class UserPageController : Controller
 {
     private readonly ILogger<UserPageController> _logger;
+    private readonly IUserRepository _userRepository;
+    private readonly UserManager<IdentityUser> _userManager;
 
-    public UserPageController(ILogger<UserPageController> logger)
+    public UserPageController(ILogger<UserPageController> logger, IUserRepository userRepository, UserManager<IdentityUser> userManager)
     {
         _logger = logger;
+        _userRepository = userRepository;
+        _userManager = userManager;
     }
 
-    public IActionResult Index()
+    [Authorize]
+    [HttpGet]
+    public IActionResult Index(UserInfoModel userInfoModel)
     {
-        return View();
+        string identityId = _userManager.GetUserId(User) ?? "";
+        Models.User gymBroUser = _userRepository.GetUserByIdentityUserId(identityId);
+        userInfoModel.Username = gymBroUser.Username;
+        userInfoModel.Email = gymBroUser.Email;
+        userInfoModel.FirstName = gymBroUser.FirstName;
+        userInfoModel.LastName = gymBroUser.LastName;
+        userInfoModel.WorkoutPlans = _userRepository.GetWorkoutPlansByIdentityUserId(identityId);
+        return View(userInfoModel);
+
+        // return View();
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
