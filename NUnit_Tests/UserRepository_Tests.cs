0586180bd5a1b5c775d37c5d44118117bd6b1f28
Angel Larios <alarios21@mail.wou.edu>
2025-02-16 11:39:14 -0800

    Added more unit tests to 'UserRepository_Tests' to test repository methods

--- NUnit_Tests/UserRepository_Tests.cs
+++ NUnit_Tests/UserRepository_Tests.cs
@@ -7,7 +7,6 @@ using GymBro_App.Models;
 public class UserRepository_Tests
 {
     private Mock<GymBroDbContext> _mockContext;
-    private Mock<DbSet<User>> _mockSet;
     private List<WorkoutPlan> _workoutPlans;
     private List<User> _users;
 
@@ -26,11 +25,6 @@ public class UserRepository_Tests
     {
         _mockContext = new Mock<GymBroDbContext>();
 
-        _users = new List<User>
-        {
-            new User { UserId = 1, IdentityUserId = "FakeIdentityId1", Username = "Test_User1", Email = "test@email.com", Password = "Don't store passwords here", Age = 42, Gender = "Male", FitnessLevel = "Advanced", Height = 6.0M, Weight = 200.0M, Fitnessgoals = "Build muscle", AccountCreationDate = new DateTime(2025, 1, 1), LastLogin = new DateTime(2025, 1, 1), PreferredWorkoutTime = "Morning", Location = "Test Location"},
-        };
-
         _workoutPlans = new List<WorkoutPlan>
         {
             new WorkoutPlan { UserId = 1, PlanName = "First Plan", StartDate = new DateOnly(2025, 1, 1), EndDate = new DateOnly(2025, 3, 1), Frequency = "3 times a week", Goal = "Build muscle", IsCompleted = 1, DifficultyLevel = "Beginner" },
@@ -38,13 +32,73 @@ public class UserRepository_Tests
             new WorkoutPlan { UserId = 1, PlanName = "Third Plan", StartDate = new DateOnly(2025, 1, 1), EndDate = new DateOnly(2025, 1, 25), Frequency = "2 times a week", Goal = "Build muscle", IsCompleted = 0, DifficultyLevel = "Advanced" },
         };
 
-
-        
+        _users = new List<User>
+        {
+            new User { UserId = 1, IdentityUserId = "FakeIdentityId1", Username = "Test_User1", Email = "test@email.com", Password = "Don't store passwords here", Age = 42, Gender = "Male", FitnessLevel = "Advanced", Height = 6.0M, Weight = 200.0M, Fitnessgoals = "Build muscle", AccountCreationDate = new DateTime(2025, 1, 1), LastLogin = new DateTime(2025, 1, 1), PreferredWorkoutTime = "Morning", Location = "Test Location", WorkoutPlans = _workoutPlans }
+        };
     }
 
     [Test]
     public void GetUserByIdentityUserId_ShouldReturnUser()
     {
-        return;
+        // Arrange
+        var mockSet = GetMockDbSet(_users.AsQueryable());
+        _mockContext.Setup(c => c.Users).Returns(mockSet.Object);
+        var repository = new UserRepository(_mockContext.Object);
+
+        // Act
+        var result = repository.GetUserByIdentityUserId("FakeIdentityId1");
+
+        // Assert
+        string expectedUsername = "Test_User1";
+        Assert.That(expectedUsername, Is.EqualTo(result.Username));
+    }
+
+    [Test]
+    public void GetWorkoutPlansByIdentityUserId_ShouldReturnWorkoutPlans()
+    {
+        // Arrange
+        var mockSet = GetMockDbSet(_users.AsQueryable());
+        _mockContext.Setup(c => c.Users).Returns(mockSet.Object);
+        var repository = new UserRepository(_mockContext.Object);
+
+        // Act
+        var result = repository.GetWorkoutPlansByIdentityUserId("FakeIdentityId1");
+
+        // Assert
+        int expectedCount = 3;
+        Assert.That(expectedCount, Is.EqualTo(result.Count));
+    }
+
+    [Test]
+    public void GetWorkoutPlansByIdentityUserIdIntOverload_ShouldReturnCompletedWorkoutPlans()
+    {
+        // Arrange
+        var mockSet = GetMockDbSet(_users.AsQueryable());
+        _mockContext.Setup(c => c.Users).Returns(mockSet.Object);
+        var repository = new UserRepository(_mockContext.Object);
+
+        // Act
+        var result = repository.GetWorkoutPlansByIdentityUserId("FakeIdentityId1", 1);
+
+        // Assert
+        int expectedCount = 2;
+        Assert.That(expectedCount, Is.EqualTo(result.Count));
+    }
+
+    [Test]
+    public void GetWorkoutPlansByIdentityUserIdIntOverload_ShouldReturnUncompletedWorkoutPlans()
+    {
+        // Arrange
+        var mockSet = GetMockDbSet(_users.AsQueryable());
+        _mockContext.Setup(c => c.Users).Returns(mockSet.Object);
+        var repository = new UserRepository(_mockContext.Object);
+
+        // Act
+        var result = repository.GetWorkoutPlansByIdentityUserId("FakeIdentityId1", 0);
+
+        // Assert
+        int expectedCount = 1;
+        Assert.That(expectedCount, Is.EqualTo(result.Count));
     }
 }
