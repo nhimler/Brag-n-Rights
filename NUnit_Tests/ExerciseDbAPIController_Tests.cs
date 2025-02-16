cd10ade43d305a9987cd29658a8bf68942f8ed0e
Nicholas Himler <nhimler20@mail.wou.edu>
2025-02-15 20:26:35 -0800

    NUnit Tests for API Controller

---
+++ NUnit_Tests/ExerciseDbAPIController_Tests.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GymBro_App.Controllers;
+using GymBro_App.Models.DTOs;
+using GymBro_App.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+
+namespace ExerciseDbAPIControllerTests
+{
+    public class ExerciseDbAPIController_Tests
+    {
+        private Mock<IExerciseService> _mockService;
+        private Mock<ILogger<ExerciseDbAPIController>> _mockLogger;
+        private ExerciseDbAPIController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockService = new Mock<IExerciseService>();
+            _mockLogger = new Mock<ILogger<ExerciseDbAPIController>>();
+            _controller = new ExerciseDbAPIController(_mockService.Object, _mockLogger.Object);
+        }
+
+        [Test]
+        public async Task GetExercisesAsync_ReturnsListOfExercises()
+        {
+            // Arrange
+            var exercises = new List<ExerciseDTO>
+            {
+                new ExerciseDTO { Name = "Push Up" },
+                new ExerciseDTO { Name = "Pull Up" }
+            };
+            _mockService.Setup(service => service.GetExercisesAsync()).ReturnsAsync(exercises);
+
+            // Act
+            var result = await _controller.GetExercises();
+
+            // Assert
+            Assert.IsInstanceOf<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.That(okResult.Value, Is.EqualTo(exercises));
+        }
+
+        [Test]
+        public async Task GetExercisesAsync_ReturnsNotFound_WhenNoExercises()
+        {
+            // Arrange
+            _mockService.Setup(service => service.GetExercisesAsync()).ReturnsAsync(new List<ExerciseDTO>());
+
+            // Act
+            var result = await _controller.GetExercises();
+
+            // Assert
+            Assert.IsInstanceOf<NotFoundObjectResult>(result);
+            var notFoundResult = result as NotFoundObjectResult;
+            Assert.IsNotNull(notFoundResult);
+            Assert.That(notFoundResult.Value, Is.EqualTo("No exercises found for the given query."));
+        }
+
+        [Test]
+        public async Task GetExerciseAsync_ReturnsExercise()
+        {
+            // Arrange
+            var exercises = new List<ExerciseDTO>
+            {
+                new ExerciseDTO { Name = "Push Up" }
+            };
+            _mockService.Setup(service => service.GetExerciseAsync("Push Up")).ReturnsAsync(exercises);
+
+            // Act
+            var result = await _controller.GetExercise("Push Up");
+
+            // Assert
+            Assert.IsInstanceOf<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.That(okResult.Value, Is.EqualTo(exercises));
+        }
+
+        [Test]
+        public async Task GetExerciseAsync_ReturnsNotFound_WhenNoExercise()
+        {
+            // Arrange
+            _mockService.Setup(service => service.GetExerciseAsync("NonExistentExercise")).ReturnsAsync(new List<ExerciseDTO>());
+
+            // Act
+            var result = await _controller.GetExercise("NonExistentExercise");
+
+            // Assert
+            Assert.IsInstanceOf<NotFoundObjectResult>(result);
+            var notFoundResult = result as NotFoundObjectResult;
+            Assert.IsNotNull(notFoundResult);
+            Assert.That(notFoundResult.Value, Is.EqualTo("No exercises found for 'NonExistentExercise'."));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _mockService = null;
+            _mockLogger = null;
+            _controller = null;
+        }
+    }
+}
