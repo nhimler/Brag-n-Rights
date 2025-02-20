2997180419b1a2d62679ffbebd6f4b7380046ec7
Nicholas Himler <nhimler20@mail.wou.edu>
2025-02-20 10:10:45 -0800

    Deleted Default Table, Starting to add Identity to page

--- GymBro_App/Controllers/WorkoutsController.cs
+++ GymBro_App/Controllers/WorkoutsController.cs
@@ -2,16 +2,26 @@ using Microsoft.AspNetCore.Mvc;
 using GymBro_App.Models;
 using GymBro_App.DAL.Abstract;
 using GymBro_App.Services;
+using Microsoft.AspNetCore.Identity;
 
 namespace GymBro_App.Controllers
 {
     public class WorkoutsController : Controller
     {
         private readonly IWorkoutPlanRepository _workoutPlanRepository;
+        
+        private readonly ILogger<WorkoutsController> _logger;
 
-        public WorkoutsController(IWorkoutPlanRepository workoutPlanRepository)
+        private readonly UserManager<IdentityUser> _userManager;
+
+        private readonly IUserRepository _userRepository;
+
+        public WorkoutsController(IWorkoutPlanRepository workoutPlanRepository, ILogger<WorkoutsController> logger, UserManager<IdentityUser> userManager, IUserRepository userRepository)
         {
             _workoutPlanRepository = workoutPlanRepository;
+            _logger = logger;
+            _userManager = userManager;
+            _userRepository = userRepository;
         }
 
         [HttpGet]
