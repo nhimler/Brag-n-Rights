dee7d37536b0ce1b9aeedfcfa86571e9ed648bf2
Angel Larios <alarios21@mail.wou.edu>
2025-02-21 12:16:02 -0800

    User location gets saved into database when calling putUserPosition

---
+++ GymBro_App/Models/DTOs/UserDTO.cs
@@ -0,0 +1,8 @@
+namespace GymBro_App.Models.DTOs
+{
+    public class UserDTO
+    {
+        public decimal Longitude { get; set; }
+        public decimal Latitude { get; set; }
+    }
+}
--- GymBro_App/wwwroot/js/UserPage Scripts/userLocation.js
+++ GymBro_App/wwwroot/js/UserPage Scripts/userLocation.js
@@ -1,17 +1,42 @@
-function getPositionSuccess(pos) {
-    const demoOutput = document.getElementById('for-demo-only');
-    const coordinates = pos.coords;
-    demoOutput.innerHTML = `Latitude: ${coordinates.latitude}, Longitude: ${coordinates.longitude}`;
-}
 
 function getPositionError(err) {
     console.log(`Error ${err.code}: couldn't get location. Issue: ${err.message}`);
+    return null;
+}
+
+async function putUserPosition(position) {
+    console.log("Position: ", position);
+    const coordinates = position.coords;
+    console.log(`Latitude: ${coordinates.latitude.toFixed(6)}, Longitude: ${coordinates.longitude.toFixed(6)}`);
+
+    const user = {
+        latitude : coordinates.latitude,
+        longitude : coordinates.longitude
+    }
+
+    const response = await fetch('/api/users', {
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json; application/problem+json; charset=utf-8',
+            'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: JSON.stringify(user)
+    });
+
+    console.log('Response: ', response);
+    if (response.ok) {
+        console.log('User location updated successfully.');
+    }
+    else {
+        console.log('Something went wrong when updating user location.');
+    }
 }
 
 function getUserPosition() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(getPositionSuccess, getPositionError);
+        navigator.geolocation.getCurrentPosition(putUserPosition);
     } else {
         console.log('navigator.geolocation not found.');
     }
 }
+
