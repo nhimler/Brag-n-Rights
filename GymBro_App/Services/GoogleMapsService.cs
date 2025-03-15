6162718d9a92309ebe53b7d3b7429e6c62398f8c
Angel Larios <alarios21@mail.wou.edu>
2025-03-15 13:14:32 -0700

    Added tests for GoogleMapsService; Made necessary changes to pass new tests.

--- GymBro_App/Services/GoogleMapsService.cs
+++ GymBro_App/Services/GoogleMapsService.cs
@@ -7,137 +7,6 @@ using GymBro_App.Services;
 
 namespace GymBro_App.Services
 {
-
-    // Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
-    public class AddressComponent
-    {
-        [JsonPropertyName("long_name")]
-        public string LongName { get; set; } = "";
-
-        [JsonPropertyName("short_name")]
-        public string ShortName { get; set; } = "";
-
-        [JsonPropertyName("types")]
-        public List<string> Types { get; set; } = [];
-    }
-
-    public class Bounds
-    {
-        [JsonPropertyName("northeast")]
-        public Northeast Northeast { get; set; } = new Northeast();
-
-        [JsonPropertyName("southwest")]
-        public Southwest Southwest { get; set; } = new Southwest();
-    }
-
-    public class Geometry
-    {
-        [JsonPropertyName("location")]
-        public Location Location { get; set; } = new Location();
-
-        [JsonPropertyName("location_type")]
-        public string LocationType { get; set; } = "";
-
-        [JsonPropertyName("viewport")]
-        public Viewport Viewport { get; set; } = new Viewport();
-
-        [JsonPropertyName("bounds")]
-        public Bounds Bounds { get; set; } = new Bounds();
-    }
-
-    public class Location
-    {
-        [JsonPropertyName("lat")]
-        public double Lat { get; set; }
-
-        [JsonPropertyName("lng")]
-        public double Lng { get; set; }
-
-        [JsonPropertyName("latitude")]
-        public double Latitude { get; set; }
-
-        [JsonPropertyName("longitude")]
-        public double Longitude { get; set; }
-    }
-
-    public class NavigationPoint
-    {
-        [JsonPropertyName("location")]
-        public Location Location { get; set; } = new Location();
-    }
-
-    public class Northeast
-    {
-        [JsonPropertyName("lat")]
-        public double Lat { get; set; }
-
-        [JsonPropertyName("lng")]
-        public double Lng { get; set; }
-    }
-
-    public class PlusCode
-    {
-        [JsonPropertyName("compound_code")]
-        public string CompoundCode { get; set; } = "";
-
-        [JsonPropertyName("global_code")]
-        public string GlobalCode { get; set; } = "";
-    }
-
-    public class Result
-    {
-        [JsonPropertyName("address_components")]
-        public List<AddressComponent> AddressComponents { get; set; } = [];
-
-        [JsonPropertyName("formatted_address")]
-        public string FormattedAddress { get; set; } = "";
-
-        [JsonPropertyName("geometry")]
-        public Geometry Geometry { get; set; } = new Geometry();
-
-        [JsonPropertyName("navigation_points")]
-        public List<NavigationPoint> NavigationPoints { get; set; } = [];
-
-        [JsonPropertyName("place_id")]
-        public string PlaceId { get; set; } = "";
-
-        [JsonPropertyName("plus_code")]
-        public PlusCode PlusCode { get; set; } = new PlusCode();
-
-        [JsonPropertyName("types")]
-        public List<string> Types { get; set; } = [];
-    }
-
-    public class Root
-    {
-        [JsonPropertyName("plus_code")]
-        public PlusCode PlusCode { get; set; } = new PlusCode();
-
-        [JsonPropertyName("results")]
-        public List<Result> Results { get; set; } = [];
-
-        [JsonPropertyName("status")]
-        public string Status { get; set; } = "";
-    }
-
-    public class Southwest
-    {
-        [JsonPropertyName("lat")]
-        public double Lat { get; set; }
-
-        [JsonPropertyName("lng")]
-        public double Lng { get; set; }
-    }
-
-    public class Viewport
-    {
-        [JsonPropertyName("northeast")]
-        public Northeast Northeast { get; set; } = new Northeast();
-
-        [JsonPropertyName("southwest")]
-        public Southwest Southwest { get; set; } = new Southwest();
-    }
-
     public class GoogleMapsService : IGoogleMapsService
     {
         readonly HttpClient _httpClient;
@@ -159,11 +28,12 @@ namespace GymBro_App.Services
         public async Task<string> ReverseGeocode(double latitude, double longitude)
         {
             string apiKey = _httpClient.DefaultRequestHeaders.GetValues("X-goog-api-key").FirstOrDefault() ?? "";
-            string url = $"https://maps.googleapis.com/maps/api/geocode/json?latlng={latitude},{longitude}&key={apiKey}";
-            string response = await _httpClient.GetStringAsync(url);
-            if (response != null)
+            string url = $"api/geocode/json?latlng={latitude},{longitude}&key={apiKey}";
+            var response = await _httpClient.GetAsync(url);
+            if (response.IsSuccessStatusCode)
             {
-                Root myDeserializedClass = JsonSerializer.Deserialize<Root>(response) ?? new Root();
+                var responseContent = await response.Content.ReadAsStringAsync();
+                ReverseGeocodeDTO myDeserializedClass = JsonSerializer.Deserialize<ReverseGeocodeDTO>(responseContent) ?? new ReverseGeocodeDTO();
                 return myDeserializedClass.Results[0].FormattedAddress;
             }
             else
@@ -171,5 +41,7 @@ namespace GymBro_App.Services
                 return "";
             }
         }
+
+        
     }
 }
